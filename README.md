f547d3b4c6c0d7c8bf788bbbc8a4030d2a279042
ssm512 <shinsang512@naver.com>
2026-04-15 17:46:27 +0900

    20260415 hr 부서명, 직원이름, 전화번호 받아오기 2nd commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # PrjDB 프로젝트
+
+## 드라이버 다운로드 경로
+https://mvnrepository.com/artifact/com.oracle.database.jdbc/ojdbc11/23.26.1.0.0
+검색 : oracle
+		oracle11
+			lastest version 23.26.1.0.0
+				Files : jar(7.3Mb) 다운로드
+				
+sts project : PrjDB 생성
+lib 폴더 만들고
+	 ojdbc11-23.26.1.0.0.jar 붙여넣고
+	 PrjDB properties -> java build path -> libraries -> add jars -> apply
+	 하면 Referenced Librabies 추가 되고 lib 밑에 jar 파일의 아이콘도 바뀜
--- src/db01/Prac01.java
+++ src/db01/Prac01.java
@@ -1,5 +1,11 @@
 package db01;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Prac01 {
 
 	private	static	String	driver	=	"oracle.jdbc.OracleDriver";
@@ -7,10 +13,28 @@ public class Prac01 {
 	private	static	String	dbuid	=	"hr";
 	private	static	String	dbpwd	=	"1234";
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		// hr 스키마에서 부서명, 직원이름, 전화번호 출력
+		Class.forName(driver); 
+		Connection	conn	=	DriverManager.getConnection(dburl, dbuid, dbpwd);
 		
+		Statement	stmt	=	conn.createStatement();
+		String		sql		=	"SELECT D.department_name	DNAME, E.first_name || ' ' || E.last_name  ENAME, E.phone_number PHONE " + 
+								" FROM departments D JOIN employees E ON D.department_id = E.department_id"; 
+		//System.out.println(sql);
+		ResultSet	rs		=	stmt.executeQuery(sql);
+		System.out.println("부서명, 직원이름, 전화번호");
+		while( rs.next() != false ) {
+			System.out.print(rs.getString("DNAME") + ',');
+			System.out.print(rs.getString("ENAME") + ',');
+			System.out.print(rs.getString("PHONE"));
+			System.out.println();
+		}// while 문 end
+		
+		rs.close();
+		stmt.close();
+		conn.close();
 
-	}
+	} // main end
 
-}
+} // class Prac01 end
